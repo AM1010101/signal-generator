4a2f8decd00cef25a67a58eb9c166b459eefde64
Andrew Merrin <78210129+AM1010101@users.noreply.github.com>
2023-05-16 22:21:19 +0100

    added waveforms

--- table.py
+++ table.py
@@ -11,14 +11,16 @@ class WavefromGenerator:
 
     
     def render_page(self):
+        st.title('Waveform Generator')
+        st.write('This `Waveform Generator` allows you to specify a number of waves and then adds them together.')
         df = pd.DataFrame()
         wavetype = pd.DataFrame(
             {"command": ["Sine", "Square", "Triangle", "Sawtooth"]}
         )
         df["Frequency"] = [3.0, 2.0, 3.0]
-        df["Amplitude"] = [1, 1, 1]
-        df["Offset"] = [1, 4, 3]
-        df["Duration"] = [1, 1, 1]
+        df["Amplitude"] = [1.0, 1.0, 1.0]
+        df["Offset"] = [1.0, 4.0, 3.0]
+        df["Duration"] = [1.0, 1.0, 1.0]
         df["Wavetype"] = (
             wavetype["command"].astype("category")
         )
@@ -26,37 +28,43 @@ class WavefromGenerator:
         edited_df = st.experimental_data_editor(df, key="my_df", num_rows='dynamic')
 
         self.sample_rate = st.slider('Sample Rate', 1, 100, 40, 1)
-        silence_duration = st.slider('Zero Gap', 0.0, 2.0, 1.0, 0.1)
+        silence_duration = st.slider('Gap Duration(sets wave to zero)', 0.0, 2.0, 1.0, 0.05)
 
         if st.button('Generate'):
             frequencies = edited_df['Frequency'].values
             amplitudes = edited_df['Amplitude'].values
             offsets = edited_df['Offset'].values
             durations = edited_df['Duration'].values
-            st.session_state.current_waveform = self.generate_waveform(frequencies, amplitudes, offsets, durations, sr=self.sample_rate, silence_duration=silence_duration)
-            st.button('Save', on_click=self.save_waveform(st.session_state.current_waveform, self.sample_rate, 'test'))
+            wave_types = edited_df['Wavetype'].values
+            st.session_state.current_waveform = self.generate_waveform(frequencies, amplitudes, offsets, durations, sr=self.sample_rate, silence_duration=silence_duration, wavetypes=wave_types)
+            st.button('Save', on_click=self.save_waveform(st.session_state.current_waveform, self.sample_rate, 'waveform_data'))
         
         if st.session_state.current_waveform is not None:
             self.plot_waveform(st.session_state.current_waveform)
 
-        
-        
-        
-    
 
-    def generate_waveform(self,frequencies, amplitudes, offsets, durations, sr, silence_duration):
+    def generate_waveform(self,frequencies, amplitudes, offsets, durations, sr, silence_duration, wavetypes):
         # Calculate the total number of samples for the entire waveform
         total_samples = int(sr * (sum(durations) + len(durations) * silence_duration))
         # Create an empty array to store the waveform
         waveform = np.zeros(total_samples)
         # Create each sine wave and silence period, and add them to the waveform
         start = 0
-        for freq, amp, offset, dur in zip(frequencies, amplitudes, offsets, durations):
+        for freq, amp, offset, dur, wt in zip(frequencies, amplitudes, offsets, durations, wavetypes):
             samples = int(sr * dur)
             t = np.linspace(0, dur, samples, endpoint=False)
             # check the wavetype and generate appropriate waveform
-            sine_wave = amp * np.sin(2 * np.pi * freq * t) + offset
-            waveform[start:start+samples] += sine_wave
+            if wt == 'Sine':
+                wave = amp * np.sin(2 * np.pi * freq * t) + offset
+            elif wt == 'Square':
+                wave = amp * np.sign(np.sin(2 * np.pi * freq * t)) + offset
+            elif wt == 'Triangle':
+                wave = amp * np.arcsin(np.sin(2 * np.pi * freq * t)) + offset
+            elif wt == 'Sawtooth':
+                wave = amp * np.arctan(np.tan(2 * np.pi * freq * t)) + offset
+            else:
+                wave = amp * np.sin(2 * np.pi * freq * t) + offset
+            waveform[start:start+samples] += wave
             start += samples
             # Add a silence period
             silence_samples = int(sr * silence_duration)
@@ -91,7 +99,6 @@ class WavefromGenerator:
                 writer.writerow([timestamps[i], waveform[i]])
 
         
-
 if __name__ == "__main__":
     waveform_generator = WavefromGenerator()
     
